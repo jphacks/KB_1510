18c91a9157a4cb2ce6a5adbf24b0bf2e12b7e187
masa_surface <masa.kasajima@gmai.com>
2015-10-17 19:41:19 +0900

    パス修正

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,7 +1,7 @@
 <?php
 
 App::uses('AppModel', 'Model');
-App::uses('BlowfishPasswordHasher', 'Controller/component/Auth');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel{
   public $validate = array(
