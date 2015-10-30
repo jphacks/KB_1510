7ec21469c2f49cbc5151bfa73754d053981709fe
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2015-10-30 09:03:17 +0900

    必要なsql分をこちらへ追加

---
+++ requirement.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `users` DROP `role`;
+
+ALTER TABLE `teachers` ADD `programing_skill` VARCHAR(250) NOT NULL AFTER `language`, ADD `price` INT NOT NULL AFTER `programing_skill`, ADD `introduction` VARCHAR(250) NOT NULL AFTER `price`;
+
+ALTER TABLE `teachers` ADD `desire_time` VARCHAR(250) NOT NULL AFTER `introduction`, ADD `way` VARCHAR(250) NOT NULL AFTER `desire_time`;
+
+ALTER TABLE `teachers` ADD `old` INT NOT NULL AFTER `way`;
+
