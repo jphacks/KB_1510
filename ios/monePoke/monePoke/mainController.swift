6d5fa49201dcd316b08dfa6fc19503b63bb4c8e9
Yuki-Ikegami <fp25657.hawks@icloud.com>
2015-11-28 13:16:39 +0900

    before lunch

--- ios/monePoke/monePoke/mainController.swift
+++ ios/monePoke/monePoke/mainController.swift
@@ -9,11 +9,33 @@
 import UIKit
 import MapKit
 
-class mainController: UIViewController {
+class mainController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var plabel: UILabel!
+    var myLocation:CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pointLabel.frame = CGRectMake(0, UIScreen.mainScreen().bounds.height/15, UIScreen.mainScreen().bounds.width * 4 / 5, mapView.frame.minY - UIScreen.mainScreen().bounds.height/15)
         
+        plabel.center = CGPoint(x: pointLabel.frame.maxX + 25, y: pointLabel.frame.midY + 20)
+        
+        myLocation = CLLocationManager()
+        myLocation.delegate = self
+        myLocation.distanceFilter = 1.0
+        myLocation.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == CLAuthorizationStatus.NotDetermined {
+            self.myLocation.requestAlwaysAuthorization()
+        }
+        
+        myLocation.startUpdatingLocation()
+        mapView.frame = CGRectMake(0, UIScreen.mainScreen().bounds.height/5, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height - tabBar.frame.height - UIScreen.mainScreen().bounds.height/5)
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +43,26 @@ class mainController: UIViewController {
         
     }
 
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let myLat: CLLocationDegrees = manager.location!.coordinate.latitude
+        let myLon: CLLocationDegrees = manager.location!.coordinate.longitude
+        let myConrdinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon) as CLLocationCoordinate2D
+        
+        print("my location is [\(myLat):\(myLon)]")
+        
+        let myLatDist:CLLocationDistance = 300
+        let myLonDist:CLLocationDistance = 300
+        
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myConrdinate, myLatDist, myLonDist)
+        
+        mapView.setRegion(myRegion, animated: true)
+        
+    }
 
 }
 
