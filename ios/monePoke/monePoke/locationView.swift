5f452aa74f2e6adfdfc68c19de9c46d4b4059ead
Yuki-Ikegami <fp25657.hawks@icloud.com>
2015-11-28 11:45:13 +0900

    complete ios setting

--- ios/monePoke/monePoke/locationView.swift
+++ ios/monePoke/monePoke/locationView.swift
@@ -1,26 +1,20 @@
 //
-//  ViewController.swift
+//  locationView.swift
 //  monePoke
 //
 //  Created by IkegamiYuki on 11/28/15.
 //  Copyright © 2015 IkegamiYuki. All rights reserved.
 //
 
+import Foundation
 import UIKit
-import MapKit
 
-class mainController: UIViewController {
-
+class locationView:UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
     }
-
-
 }
-
